0aab816be60cf283ac3cace8b46246261cbb25a6
yunzhao <408958321@qq.com>
2017-06-15 13:38:34 +0800

    add queue.js mod unique_array.js

---
+++ jspractice/queue.js
@@ -0,0 +1,61 @@
+/**
+ * Created by Momax on 2017/6/15.
+ * 队列的实现
+ */
+
+(function() {
+    "use strict";
+    function Queue() {
+        this.dataStore = [];
+
+    }
+
+    Queue.prototype.enqueue = function (element) {
+        if(Array.isArray(element)){
+            var i;
+            var len = element.length;
+               for (i = 0; i < len; i += 1){
+                    this.dataStore.push(element[i]);
+               }
+        }else {
+            this.dataStore.push(element);
+        }
+    };
+
+    Queue.prototype.dequeue = function () {
+        return this.dataStore.shift();
+    };
+
+    Queue.prototype.front = function () {
+        return this.dataStore[0];
+    };
+    Queue.prototype.back = function () {
+        var len = this.dataStore.length - 1;
+        return this.dataStore[len];
+    };
+    Queue.prototype.toString = function () {
+        var reStr = "";
+        var i;
+        var len = this.dataStore.length;
+        for (i = 0; i < len; i += 1){
+            reStr += this.dataStore[i] + "\n";
+        }
+        return reStr;
+    };
+    Queue.prototype.empty = function (){
+        var len = this.dataStore.length;
+        return len === 0;
+    };
+
+    var a = ["yun","zhao","cc","hehe"];
+    var q = new Queue();
+    q.enqueue("hahhah");
+    q.enqueue("momax");
+    q.enqueue(a);
+    console.log(q.toString());
+    console.log(q.dequeue());
+    console.log(q.empty());
+    console.log(q.front());
+    console.log(q.back());
+}());
+
--- my-plug-in/tool-function/unique_array.js
+++ my-plug-in/tool-function/unique_array.js
@@ -6,16 +6,16 @@
 function unique(array){
     "use strict";
     var arr = [];
-    var len =array.legth;
+    var len = array.legth;
     var i;
-    for ( i = 0; i < len; i += 1) {
-        if(arr.indexOf(array[i]) === -1){
+    for (i = 0; i < len; i += 1) {
+        if (arr.indexOf(array[i]) === -1) {
             arr.push(array[i]);
         }
     }
     return arr;
 }
 
-var array = [12,2,3,3,4,5,3,4,5,68,8,8];
+var array = [12, 2, 3, 3, 4, 5, 3, 4, 5, 68, 8, 8];
 
 unique(array);
