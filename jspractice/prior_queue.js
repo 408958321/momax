8c007fedce4f24d1ceaa53f79f5922f8f225befb
yunzhao <408958321@qq.com>
2017-07-06 14:53:55 +0800

    mod prior_queue

--- jspractice/prior_queue.js
+++ jspractice/prior_queue.js
@@ -1,96 +1,91 @@
 /**
  * Created by momax on 2017/7/4.
  * 优先队列
+ * 1优先级最高
  */
 
-(function(){
+(function () {
     "use strict";
-    function priorQueue() {
+    var i, len;
+
+    function PriorQueue() {
         this.dataStore = [];
     }
 
-    priorQueue.prototype.enqueue = function (element) {
-        if (Array.isArray(element)) {
-            var i;
-            var len = element.length;
-            for (i = 0; i < len; i += 1) {
-                this.dataStore.push(element[i]);
-            }
-        } else {
-            this.dataStore.push(element);
-        }
-    };
-
-    priorQueue.prototype.dequeue = function () {
-        var priority = this.dataStore[0].code;
-        var i;
-        var len = this.dataStore.length;
-        for (i = 1; i < len; i += 1) {
-            if (this.dataStore[i].code < priority) {
-                priority = i;
-            }
-        }
-        return this.dataStore.splice(priority,1);
-    };
-
-    priorQueue.prototype.front = function () {
-        return this.dataStore[0];
-    };
-
-    priorQueue.prototype.back = function () {
-        var len = this.dataStore.length - 1;
-        return this.dataStore[len];
-    };
-
-    priorQueue.prototype.toString = function () {
-        var reStr = "";
-        var i;
-        var len = this.dataStore.length;
-        for (i = 0; i < len; i += 1) {
-            reStr += this.dataStore[i].name + " code: " +
-                     this.dataStore[i].code + "\n";
-        }
-        return reStr;
-    };
-
-    priorQueue.prototype.empty = function () {
-        var len = this.dataStore.length;
-        return len === 0;
-    };
-
-    priorQueue.prototype.count = function () {
-        return this.dataStore.length;
-    };
-
-
-    function Patient (name, code) {
+    function Patient(name, code) {
         this.name = name;
         this.code = code;
     }
 
-    var p = new Patient ("Smith", 5);
-    var ed = new priorQueue();
-    ed.enqueue(p);
-    p = new Patient("Jones", 4);
-    ed.enqueue(p);
-    p = new Patient("Brown", 1);
-    ed.enqueue(p);
-    p = new Patient("Ingram", 1);
+    PriorQueue.prototype = {
+        constructor: PriorQueue,
+        enqueue: function (element) {
+            if (Array.isArray(element)) {
+                len = element.length;
+                for (i = 0; i < len; i += 1) {
+                    this.dataStore.push(element[i]);
+                }
+            } else {
+                this.dataStore.push(element);
+            }
+        },
+        dequeue: function () {
+            var priority = this.dataStore[0].code;
+            var index;
+            len = this.dataStore.length;
+            for (i = 1; i < len; i += 1) {
+                if (this.dataStore[i].code < priority) {
+                    priority = this.dataStore[i].code;
+                    index = i;
+                }
+            }
+            return this.dataStore.splice(index, 1);
+        },
+        toString: function () {
+            var reStr = "";
+            len = this.dataStore.length;
+            for (i = 0; i < len; i += 1) {
+                reStr += this.dataStore[i].name + " code:" +
+                    this.dataStore[i].code + "\n";
+            }
+            return reStr;
+        },
+        front: function () {
+            return this.dataStore[0];
+        },
+        back: function () {
+            len = this.dataStore.length - 1;
+            return this.dataStore[len];
+        },
+        empty: function () {
+            len = this.dataStore.length;
+            return len === 0;
+        },
+        count: function () {
+            return this.dataStore.length;
+        }
+    };
+    var ed = new PriorQueue();
+    var p = [new Patient("Smith", 1), new Patient("Jones", 1), new Patient("Brown", 1), new Patient("Ingram", 1)];
     ed.enqueue(p);
 
-    console.log("total patient");
-    console.log(ed.toString());
-    var seen = ed.dequeue();
-    console.log("Patients being treated:" + seen[0].name);
-    console.log("Patients waiting to be seen: ");
-    console.log(ed.toString);
+    function inform(queue) {
 
-    //下一轮
-    var seen = ed.dequeue();
-    console.log("Patient being treated:" + seen[0].name);
-    console.log("patinents wating to be seen: ");
-    console.log(ed.toString());
+        var pat;
 
+
+        while (!queue.empty()) {
+
+            pat = queue.dequeue();
+            console.log("Patient being treated " + pat[0].name);
+            console.log("Patients waiting to be seen: ");
+            console.log(queue.toString());
+
+        }
+
+    }
+
+    inform(ed);
 
 
 }());
