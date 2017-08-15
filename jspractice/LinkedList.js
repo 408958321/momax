bd4e2ff68cc30f2e14ad916d4f8dc32772e5b2f0
momax <408958321@qq.com>
2017-08-15 08:02:27 +0800

    add

---
+++ jspractice/LinkedList.js
@@ -0,0 +1,69 @@
+//實現鏈表
+
+//實現node類 element 保存節點上的數據,next保存指向下一個節點的鏈接
+
+function Node(element) {
+    this.element = element;
+    this.next = null;
+}
+
+
+// LListl類提供對鏈表操作的方法
+
+function LList() {
+    this.head = new Node("head");
+    //this.find = find;
+   // this.insert = insert;
+   // this.remove = remove;
+   // this.display = display;
+}
+
+LList.prototype.find = function(item) {
+    var currNode = this.head;
+    while (currNode.element != item) {
+
+        currNode = currNode.next;
+    }
+    return currNode;
+}
+
+LList.prototype.insert = function(newElement, item){
+    var newNode = new Node(newElement);
+    var current = this.find(item);
+    newNode.next = current.next;
+    current.next = newNode;
+}
+
+LList.prototype.display = function() {
+    var currNode = this.head;
+    while (!(currNode.next === null)) {
+        console.log(currNode.next.element);
+        currNode = currNode.next;
+    }
+}
+
+LList.prototype.findPrevious = function(item) {
+    var currNode = this.head;
+    while (!(currNode.next === null) &&
+            (currNode.next.element !== item)) {
+        currNode = currNode.next;
+    }
+    return currNode;
+}
+
+LList.prototype.remove = function(item) {
+    var prevNode = this.findPrevious(item);
+    if(!(prevNode.next === null)) {
+        prevNode.next = prevNode.next.next;
+    }
+}
+
+var cities = new LList();
+cities.insert("Conway", "head");
+cities.insert("Russellville", "Conway");
+cities.insert("Carlisle","Russellville");
+cities.insert("Alma","Carlisle");
+
+cities.display();
+cities.remove("Carlisle");
+cities.display();
